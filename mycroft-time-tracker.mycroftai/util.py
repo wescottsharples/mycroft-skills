d84c02a7967b8a9d20ac0cca0cb63808ea1bbb83
kai-dg <haruspace.dev@gmail.com>
2019-06-07 15:45:05 -0700

    tested and updated write_data

--- mycroft-time-tracker.mycroftai/util.py
+++ mycroft-time-tracker.mycroftai/util.py
@@ -18,24 +18,20 @@ def read_data():
 def write_data(data=None, newdata=None):
     """Updates current data and writes to projects.json.
 
-    This function writes to only 1 key 1 value.
+    This function will update all key to new values in current projects.json.
 
     Args:
         data (dict): Current projects.json data.
         newdata (dict): New data to update to projects.json.
     """
-    # Manual way
-    key = list(newdata)[0]
-    for k, v in data.items():
-        if k == key:
-            v = newdata.get(key)
+    newkeys = list(newdata)
+    for k in data.keys():
+        for i in newkeys:
+        if k == i:
+            data[k] = newdata[i]
 
     with open("projects.json", "w") as wf:
         json.dump(data, wf)
-        
-    # Untested way
-    with open("projects.json", "w") as wf:
-        json.dump(newdata, wf)
         
 def data_checker():
     """Checks if projects data exists (projects.json). If it does not exist,
